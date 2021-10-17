2dc5002ef437f4ac63d370c3cc7627b5c9f79991
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2021-10-17 17:14:59 +0200

    Fix version number

--- R/twitterbot-package.R
+++ R/twitterbot-package.R
@@ -2,7 +2,7 @@
 #' \tabular{ll}{
 #'  **Package:** \tab twitterbot \cr
 #'  **Type:** \tab Package \cr
-#'  **Version:** \tab 0.1 \cr
+#'  **Version:** \tab 0.0.0.9000 \cr
 #'  **License:** \tab GPL-3 \cr
 #' }
 #' @author
